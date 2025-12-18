98c4639c1a50f27dbc54c8a13dafe6e86ad819bd
ay0301 <ayoung030113@gmail.com>
2025-12-18 10:55:52 +0900

    홈 이미지

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import styles from "./Pages.module.css";
 import Bar from "../components/Bar";
+import image1 from "../assets/image1.jpg";
 
 function Home() {    
   return (
-    <div>
+    <div className={styles.wrapper}>
       <div className={styles.mainText}>
         🎂 Happy 2nd Anniversary 🎉
       </div>
       <Bar/>
+      <div className={styles.homeImage}>
+        <img src={image1} alt="main-image" />
+      </div>
     </div>
   );
 }
